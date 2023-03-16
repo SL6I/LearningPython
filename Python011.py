71cdf6fd186ebb4a506fe62a25e24b4aeb575e24
SL6I <s443004498@st.uqu.edu.sa>
2023-03-16 10:10:25 +0300

    Strings

---
+++ Python011.py
@@ -0,0 +1,15 @@
+# ----------
+# Strings 
+# ----------
+
+myString = 'This is Single quote'
+myString = "This is Double quote"
+Str = ' Hi "Bye"'
+
+print(myString + Str)
+
+manyLines = """WELCOME 
+IN \\\"its okay to do that"  
+PYTHON"""
+# Note that We dont need to add Escape (\) At The end Of any Line .
+print(manyLines)
