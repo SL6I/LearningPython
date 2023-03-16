8228aa0609fecd2bd545e3ac46780b0d6a47ced8
SL6I <s443004498@st.uqu.edu.sa>
2023-03-16 09:44:06 +0300

    Concatention

---
+++ Python010.py
@@ -0,0 +1,22 @@
+# ---------------
+# Concatenation
+# ---------------
+
+msg = "I love "
+lang = "Python"
+print(msg+lang)
+
+full = msg+lang
+print(full)
+
+a = "Hi \
+die \
+bye "
+# --------------
+b = "A \
+B \
+C"
+print(a+b)
+
+# You cannot Concatenate String + int  --> Python x
+# print("Hi" + 1) >> Error
