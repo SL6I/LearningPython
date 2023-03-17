98e13c42ae1e48a728eb3edd7902daea4625fc96
SL6I <s443004498@st.uqu.edu.sa>
2023-03-17 14:51:45 +0300

    first project in pyrhon

---
+++ Project1-10.py
@@ -0,0 +1,16 @@
+# Hello this is a comment
+# -----------------------
+
+name =  "Sultan"
+age = "20" # i made it String to Concatenate 
+Country = "SA"
+
+print("My name is :"+name)
+print("I am "+age)
+print("I live in " + Country)
+
+print("Hello I'm "+name+" "+age+" years and I live in "+Country)
+
+print(type(name))
+print(type(age))
+print(type(Country))
