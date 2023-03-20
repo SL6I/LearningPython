78386aa1fec754c8a2875295ec963bc506757f08
SL6I <s443004498@st.uqu.edu.sa>
2023-03-21 00:30:30 +0300

    List

---
+++ Python021.py
@@ -0,0 +1,24 @@
+# lists
+# ---------------
+
+myList = [1,2,3,"Hi bye",True] # Different Data type just in python
+# Mutable > u Can Delete and add or Edit
+
+print(myList)
+print(myList[-1])
+
+print(myList[1:4]) # The same as String
+
+print(type(myList[1])) # Integer
+
+print(myList[::2])
+
+myList[2] = 2
+myList[-1] = False
+#
+myList[0:3] = [3,3,3,2] # Not replace 
+#
+print(myList)
+
+
+
