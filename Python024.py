0418cacb0c7d6b08d8947f1284387e8ebb7dc333
SL6I <s443004498@st.uqu.edu.sa>
2023-03-23 23:59:02 +0300

    Tuple

--- Python024.py
+++ Python024.py
@@ -6,7 +6,14 @@ myTT = "Bro" , "without tuple"
 print(type(myTT))
 
 # tuple indexing
-myttt = [1,2,3,4,5,6,7]
+myttt = (1,2,3,4,5,6,7)
 print(myttt[-1])
 
 # You cant edit tuple
+mytttt = (1,2,3,4,5,6,7)
+# mytttt[1] = 0
+# print(mytttt) 'tuple' object does not support item assignment
+
+# Diffrent type
+myttttt = ("A","A",1,2,3,True,10.5)
+print(myttttt[-1])
