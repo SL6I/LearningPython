0638938b38e7e585773ec35e24775182a53e8c69
SL6I <s443004498@st.uqu.edu.sa>
2023-03-23 23:55:04 +0300

    Tuple

---
+++ Python024.py
@@ -0,0 +1,12 @@
+# Tuples
+# ()
+myT = ("Sl6","ali")
+print(type(myT))
+myTT = "Bro" , "without tuple"
+print(type(myTT))
+
+# tuple indexing
+myttt = [1,2,3,4,5,6,7]
+print(myttt[-1])
+
+# You cant edit tuple
