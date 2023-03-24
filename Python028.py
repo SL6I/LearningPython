c54a166abc1816ae17f8aa04e1733077d6e1b858
SL6I <s443004498@st.uqu.edu.sa>
2023-03-24 20:21:15 +0300

    Set Method p2

---
+++ Python028.py
@@ -0,0 +1,19 @@
+# SET Method
+
+# difference() > a - b
+a = {1,2,3}
+b = {2,4,3}
+print(a.difference(b))
+
+print("-"*40)
+
+# difference_update() The set is changing
+
+
+# intersection() the similar element a & b
+
+# intersection_update() change
+
+# symmetric_difference() المختلفة i ^ g
+
+# symmetric_difference_update()
