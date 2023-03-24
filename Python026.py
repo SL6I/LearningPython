9936a03047490a96d49a87f9e4e6a2d71dc0722f
SL6I <s443004498@st.uqu.edu.sa>
2023-03-24 19:51:25 +0300

    Set

---
+++ Python026.py
@@ -0,0 +1,20 @@
+# Set {}
+
+
+# NOT indexed and NOT order
+mySet = {1,2,3,"ali"} # The order will change every time
+# print(mySet[0])  Error
+# print(mySet[1::])
+
+# Accept Only immutable Type
+# mySett  = {"Hi","Bye",1,True,[1,2]} # unhashable type: 'list'
+# print(mySett)
+
+mySett  = {"Hi","Bye",1,True,(1,2)}
+print(mySett)
+
+# Item is unique
+mySettt = {1,2,1} # Delete 1 
+print(mySettt)
+
+
