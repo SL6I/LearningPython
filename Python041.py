3a4218963ba4371eddb54a83669279649f676900
SL6I <s443004498@st.uqu.edu.sa>
2023-03-27 13:31:12 +0300

    If Else

---
+++ Python041.py
@@ -0,0 +1,14 @@
+# If Elif Else
+
+uName = "SL6"
+CC = "SA"
+cName = "Python"
+cprice = 100
+
+if CC == "SA":
+    print(cprice-50)
+    
+elif CC == "IDK":
+    print(cprice-100)
+else:
+    print(cprice-30)
