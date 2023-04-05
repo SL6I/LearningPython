0c79ad51b4f4b6a550c2b54b063e5f55cd32186a
SL6I <s443004498@st.uqu.edu.sa>
2023-04-05 18:14:50 +0300

    ****************

---
+++ Python063.py
@@ -0,0 +1,11 @@
+# Recursion
+
+def CutLetters(word):
+    if len(word) == 1:
+        return word
+    if word[0] == word[1]:
+        # print(word)
+        return CutLetters(word[1:])
+    return word[0] + CutLetters(word[1:])
+
+print(CutLetters("wwwooorrrllldd"))
