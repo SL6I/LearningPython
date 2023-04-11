7d54318b711927d1adabf30eb76e8b82852e0e3c
SL6I <s443004498@st.uqu.edu.sa>
2023-04-11 19:46:58 +0300

    Built f

---
+++ Python069.py
@@ -0,0 +1,19 @@
+# built in func
+
+# all()
+
+x  = [1,2,3,[]]
+if all(x):
+    print("All elemnts are True")
+    
+else:
+    print("Not")
+    
+# any() is the OP of all() 
+# 0 is False
+
+# bin
+
+# id the refrence
+a = 1
+print(id(a))
